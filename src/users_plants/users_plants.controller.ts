b3507fb41a4cfb2076ae540d3231cb7430761bb4
Sukhova Kira <shkiden@mail.ru>
2026-02-03 17:12:13 +0300

    feat: add user plants controller

--- src/users_plants/users_plants.controller.ts
+++ src/users_plants/users_plants.controller.ts
@@ -1,9 +1,31 @@
 import { Controller, Post, Get, Patch, Delete, Body, Param, Request, UseGuards, ParseIntPipe } from '@nestjs/common';
 import { UserPlantsService } from './users_plants.service';
+import { CreateUserPlantDto, UpdateUserPlantDto} from '../dtos/users_plants.dto'
+import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../guard/jwt-auth.guard'
 
+@ApiBearerAuth()
+@ApiTags('user plants')
+@UseGuards(JwtAuthGuard)
 @Controller('users_plants')
 export class UserPlantsController {
     constructor(private readonly userPlantsService: UserPlantsService){}
 
+    @Post()
+    add(@Body() dto: CreateUserPlantDto, @Request() req) {
+        const userId = req.user.id;
+        return this.userPlantsService.addPlantToUser(dto, userId);
+    }
     
+    @Get()
+    findAll(@Request() req) {
+        const userId = req.user.id;
+        return this.userPlantsService.findAllForUser(userId);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string, @Request() req) {
+        const userId = req.user.id;
+        return this.userPlantsService.removeUserPlant(id, userId);
+    }
 }
